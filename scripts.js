7f1e9f3f533952b74d9bd62ba8be066f65cdbd7b
DilaSaraga2007 <iunusovadildora@gmail.com>
2024-12-09 08:34:13 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+function toggleForm() {
+    const form = document.getElementById('booking-form');
+    form.style.display = (form.style.display === 'none' || form.style.display === '') ? 'block' : 'none';
+}
